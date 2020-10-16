cf51cb43091b27ba22ffe6fd98fde8ba2b1a0afd
izisoft <zinzinx8@gmail.com>
2020-10-16 11:22:03 +0700

    Update source

--- src/UrlManager.php
+++ src/UrlManager.php
@@ -5,5 +5,20 @@ use Yii;
 
 class UrlManager extends \yii\web\UrlManager
 {
+
+    /**
+     * {@inheritDoc}
+     * @see \yii\web\UrlManager::init()
+     */
     
+    public function init()
+    {
+        parent::init();
+        
+        Yii::$app->view->theme = new \izi\theme\Theme([
+            'basePath'   =>  "@app/web",
+            'viewPath'   =>  "@app/views",
+        ]);
+        
+    }
 }
