ca2a406a694423e4b618fa4a84f827da2cc523bd
A Tin <zinzinx8@gmail.com>
2020-10-24 16:10:08 +0700

    Create Translate.php

---
+++ Translate.php
@@ -0,0 +1,26 @@
+<?php 
+namespace izi\language;
+
+use Yii;
+use izi\models\SiteConfigs;
+
+class Translate extends \yii\base\Component
+
+{
+
+    /**
+     * 
+     */
+    public function translate($lang_code, $lang = __LANG__, $options = [])
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public function googleTranslate($q, $source, $target)
+    {
+
+    }
+}
